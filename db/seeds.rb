62f142e08e57f93319f54697750e77b3b4faeaac
Corey Soreff <csoreff@gmail.com>
2015-06-22 23:47:55 -0400

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(first_name: 'Corey', last_name: 'Soreff',
+  email: 'xlive4honorx@gmail.com', encrypted_password: '$2a$10$Y3Gly82slIMyrFUtFdkVmOWvUtNlcOWUJ9ePnxIOIha0FMgG5Vd8y',
+  device_token: 'ONEkbywfw1OY7P0G1NNV3Q-vmtVbi-4qZAj-FSHCi_A')
