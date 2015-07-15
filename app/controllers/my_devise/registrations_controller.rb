3759cc7557f7b8df9f1b609c0402fddb99530083
Corey Soreff <csoreff@gmail.com>
2015-07-15 11:59:09 -0400

    Removed unnecessary conditional in registration controller

--- app/controllers/my_devise/registrations_controller.rb
+++ app/controllers/my_devise/registrations_controller.rb
@@ -17,16 +17,14 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
       client = Round.client
       client.authenticate_identify(api_token: ENV['ROUND_API_TOKEN'])
       # allow(client.users).to_receive(:create)
-      if params[:user]
-        params[:user][:device_token] = client.users.create(
-          first_name: params[:user][:first_name],
-          last_name: params[:user][:last_name],
-          email: params[:user][:email],
-          passphrase: params[:user][:passphrase],
-          device_name: "device#{rand(99999)}",
-          redirect_uri: 'https://bitflash.herokuapp.com'
-        )
-      end
+      params[:user][:device_token] = client.users.create(
+        first_name: params[:user][:first_name],
+        last_name: params[:user][:last_name],
+        email: params[:user][:email],
+        passphrase: params[:user][:passphrase],
+        device_name: "device#{rand(99999)}",
+        redirect_uri: 'https://bitflash.herokuapp.com'
+      )
       # Keep original functionality of RegistrationsController
       super
     rescue StandardError
