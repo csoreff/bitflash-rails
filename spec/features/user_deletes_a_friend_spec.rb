0e543c12dbe801dfe2a3573cc6a96824b53afe2a
Corey Soreff <csoreff@gmail.com>
2015-07-14 20:06:09 -0400

    first commit

---
+++ spec/features/user_deletes_a_friend_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'user deletes a friend', %{
+  As a user
+  I want to be able to delete a friend
+  So that they no longer appear on my friends list.
+} do
+
+  scenario 'Delete a friend' do
+    user = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user2)
+
+    sign_in_as(user)
+    fill_in 'email', with: user2.email
+    click_button 'Go'
+    click_link 'Delete Friend'
+    page.driver.browser.switch_to.alert.accept
+
+    expect(page).to_not have_content(user2.first_name)
+
+  end
+end
