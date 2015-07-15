3c7fb918cfa554460cef156762ec5a93088152b1
Corey Soreff <csoreff@gmail.com>
2015-07-15 14:46:36 -0400

    transaction model specs

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,8 @@
 class Transaction < ActiveRecord::Base
   belongs_to :friendship
 
-  validates :user_id, presence: true, numericality: true
-  validates :address_id, presence: true, numericality: true
+  validates :friendship_id, presence: true, numericality: true
+  validates :sender_address_id, presence: true, numericality: true
   validates :amount, presence: true, numericality: true
   validates :recipient_address_id, presence: true, numericality: true
-  validates :recipient_user_id, presence: true, numericality: true
 end
