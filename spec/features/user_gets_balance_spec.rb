e9fb9fecb264194dfe3d343b214a98078d4510a1
Corey Soreff <csoreff@gmail.com>
2015-07-16 11:40:19 -0400

    Added method to users to check balance, created account controller and index view to check balance.

---
+++ spec/features/user_gets_balance_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User checks theirs balance', %{
+  As an authenticated user
+  I want to go to my account page
+  And see my current balance
+} do
+
+  scenario 'get balance' do
+    user = FactoryGirl.create(:user)
+
+    sign_in_as(user)
+    visit "/users/#{user.id}/account"
+
+    expect(page).to have_content("Balance: 0")
+  end
+end
