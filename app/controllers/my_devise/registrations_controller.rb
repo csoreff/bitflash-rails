bec3404dd7090532043531a1d55159137a963a82
Corey Soreff <csoreff@gmail.com>
2015-07-15 23:31:48 -0400

    Removed redundant begin block in registration controller

--- app/controllers/my_devise/registrations_controller.rb
+++ app/controllers/my_devise/registrations_controller.rb
@@ -13,24 +13,22 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
   #   end
   # end
   def create
-    begin
-      client = Round.client
-      client.authenticate_identify(api_token: ENV['ROUND_API_TOKEN'])
-      # allow(client.users).to_receive(:create)
-      params[:user][:device_token] = client.users.create(
-        first_name: params[:user][:first_name],
-        last_name: params[:user][:last_name],
-        email: params[:user][:email],
-        passphrase: params[:user][:passphrase],
-        device_name: "device#{rand(99999)}",
-        redirect_uri: 'https://bitflash.herokuapp.com'
-      )
-      # Keep original functionality of RegistrationsController
-      super
-    rescue StandardError
-      flash[:warning] = 'You were not registered successfully, please ensure all information is correct and that you have not previously registered with this email.'
-      redirect_to new_user_registration_path
-    end
+    client = Round.client
+    client.authenticate_identify(api_token: ENV['ROUND_API_TOKEN'])
+    # allow(client.users).to_receive(:create)
+    params[:user][:device_token] = client.users.create(
+      first_name: params[:user][:first_name],
+      last_name: params[:user][:last_name],
+      email: params[:user][:email],
+      passphrase: params[:user][:passphrase],
+      device_name: "device#{rand(99999)}",
+      redirect_uri: 'https://bitflash.herokuapp.com'
+    )
+    # Keep original functionality of RegistrationsController
+    super
+  rescue StandardError
+    flash[:warning] = 'You were not registered successfully, please ensure all information is correct and that you have not previously registered with this email.'
+    redirect_to new_user_registration_path
   end
 
   private
