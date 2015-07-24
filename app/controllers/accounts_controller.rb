87029b70c71a83809a6ac5fd03298d1db89dc079
Corey Soreff <csoreff@gmail.com>
2015-07-24 11:15:01 -0400

    First commit

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,4 +3,25 @@ class AccountsController < ApplicationController
     @balance = current_user.get_balance[:balance]
     @pending_balance = current_user.get_balance[:pending_balance]
   end
+
+  def new
+    @btc_price = HTTParty.get('https://www.bitstamp.net/api/ticker/')['last']
+    @friendship = Friendship.find()
+    @transaction = Transaction.new
+    @balance = current_user.get_balance[:balance]
+  end
+
+  def create
+    amount = params[:transaction][:amount].to_f * 100_000_000.to_f
+    new_transaction = make_btc_payment(
+      params[:transaction][:passphrase],
+      params[:transaction][:btcaddress],
+      amount
+    )
+    if new_transaction.mfa_uri
+      redirect_to new_transaction.mfa_uri
+    else
+      redirect_to root_path, alert: "Payment unsuccessful!"
+    end
+  end
 end
